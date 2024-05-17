cb0fa1900bb965f5c085479a489824ad8c5d2773
Jorge Reyes <92123185+JorgeArt81@users.noreply.github.com>
2024-05-16 19:26:30 -0500

    Change Home controller

--- cmd/router/controllers/main.controller.go
+++ cmd/router/controllers/main.controller.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -18,5 +19,22 @@ func Init(app *config.Application) {
 }
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, Home from %s", controllerStruct.app.Domain)
+	var payload = struct {
+		Status  string `json:"status"`
+		Message string `json:"message"`
+		Version string `json:"version"`
+	}{
+		Status:  "active",
+		Message: "Go Movies up and running",
+		Version: "1.0.0",
+	}
+
+	out, err := json.Marshal(payload)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(out)
 }
