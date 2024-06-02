8afb73c6e3f5148ee36838c4b1eba9321f7e8cfc
Jorge Reyes <92123185+JorgeArt81@users.noreply.github.com>
2024-06-01 22:09:38 -0500

    Create method authRequired

--- internal/api/middlewares.go
+++ internal/api/middlewares.go
@@ -16,3 +16,14 @@ func (s *server) enableCORS(h http.Handler) http.Handler {
 		}
 	})
 }
+
+func (s *server) authRequired(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		_, _, err := s.auth.GetTokenFromHeaderAndVerify(w, r)
+		if err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
