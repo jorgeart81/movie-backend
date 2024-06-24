7df35b09865f91c4d3bd35428cf152a47adc3b13
Jorge Reyes <92123185+JorgeArt81@users.noreply.github.com>
2024-06-24 18:24:14 -0500

    Getting a movie and genres from database

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -9,6 +9,8 @@ import (
 type DatabaseRepo interface {
 	Connection() *sql.DB
 	AllMovies() ([]*models.Movie, error)
+	OneMovie(id int) (*models.Movie, error)
+	OneMovieForEdit(id int) (*models.Movie, []*models.Genre, error)
 	GetUserByEmail(email string) (*models.User, error)
 	GetUserByID(id int) (*models.User, error)
 }
